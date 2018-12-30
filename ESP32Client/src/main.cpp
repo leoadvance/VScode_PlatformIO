f1db1ba291b4c200eead599f94df8bc8dccb311d
leoadvance <leo_advance@163.com>
2018-12-31 01:29:40 +0800

    实现HTTP get发送

--- ESP32Client/src/main.cpp
+++ ESP32Client/src/main.cpp
@@ -4,10 +4,11 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-// const char* ssid = "LEO_ASUS";
-// const char* password =  "bhu8vgy7nji9";
-const char* ssid = "LEO's iPhoneX";
-const char* password =  "8765432a";
+const char* ssid = "LEO_ASUS";
+const char* password =  "bhu8vgy7nji9";
+const char* HTTPServerAddr =  "192.168.199.140";
+const char* HTTPServerPort =  "80";
+
 int LED_BUILTIN = 2;
 // 默认Wi-Fi 未连接
 wl_status_t WiFiStatus = WL_DISCONNECTED;
@@ -15,6 +16,7 @@ wl_status_t WiFiStatus = WL_DISCONNECTED;
 #define LED_CLOSE   digitalWrite(LED_BUILTIN, LOW)
 #define LED_OPEN   digitalWrite(LED_BUILTIN, HIGH)
 
+// 字符串显示Wi-Fi状态
 char *WiFiStatusToString(int status)
 {
     switch(status)
@@ -43,7 +45,7 @@ char *WiFiStatusToString(int status)
 // 500ms定时任务
 void taskLED(void * parameter )
 {
-    //int i = 0;
+    Serial.printf("taskLED start...\r\n");
     pinMode(LED_BUILTIN, OUTPUT);
     LED_CLOSE;
     delay(500);
@@ -67,6 +69,7 @@ void taskWiFi(void * parameter )
 {
     int i = 0;
     int j = 0;
+    Serial.printf("taskWiFi start...\r\n");
     delay(500);
     while(1)
     {
@@ -77,13 +80,15 @@ void taskWiFi(void * parameter )
         {
             j = 0;
             Serial.printf("WiFiStatus = %s\r\n", WiFiStatusToString(WiFiStatus));
-        }     
+        }  
+
+        // 未联网，准备连接Wi-Fi   
         if (WiFiStatus != WL_CONNECTED)
         {
             
             Serial.printf("Connecting to Wi-Fi...\r\n");
 
-            //连接WiFi
+            //连接WiFi 并等待连接成功
             WiFi.begin(ssid, password);
             i = 20;
             do
@@ -104,6 +109,44 @@ void taskWiFi(void * parameter )
     }
 }
 
+// HTTP发送测试
+void taskHTTP(void * parameter )
+{
+    char httpURL[256];
+    int i = 0;
+    Serial.printf("taskHTTP start...\r\n");
+    while(1)
+    {
+        // Check the current connection status
+        if (WiFiStatus == WL_CONNECTED) 
+        { 
+            i++;
+            HTTPClient http;
+            memset(httpURL, 0, 256);
+            sprintf(httpURL, "http://%s:%s/temp=123&humi=456", HTTPServerAddr, HTTPServerPort);
+            Serial.printf("httpURL: %s\r\n", httpURL);
+            http.begin(httpURL); 
+            http.addHeader("Content-Type", "text/plain");  //Specify the URL
+            int httpCode = http.GET();                                        //Make the request
+
+            if (httpCode > 0) 
+            { //Check for the returning code
+                String payload = http.getString();
+                Serial.println(httpCode);
+                Serial.println(payload);
+            }
+            else 
+            {
+                Serial.printf("Error on HTTP request, httpCode = %d\r\n", httpCode);
+            }
+
+            http.end(); //Free the resources
+        }
+
+        delay(1000);   
+    }
+
+}
 void setup() 
 {
     // put your setup code here, to run once:
@@ -111,7 +154,8 @@ void setup()
     delay(200);
     Serial.println("ESP32 Client Start……");
     xTaskCreate(taskLED, "taskLED", 2048, NULL, 1, NULL);
-    xTaskCreate(taskWiFi, "taskWiFi", 8192, NULL, 2, NULL);
+    xTaskCreate(taskWiFi, "taskWiFi", 8192, NULL, 3, NULL);
+    xTaskCreate(taskHTTP, "taskHTTP", 8192, NULL, 2, NULL);
 
 }
 
@@ -127,5 +171,5 @@ void loop()
     //     }
     // }
 
-
+    delay(1000);
 }
