d377f6c93e809b68d0dc4da52dee62d03e34c878
leoadvance <leo_advance@163.com>
2018-12-30 17:40:05 +0800

    完成测试程序 增加波特率配置

--- ESP32Client/src/main.cpp
+++ ESP32Client/src/main.cpp
@@ -2,8 +2,15 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(4000);
+  Serial.println("ESP32 Client Start……");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(1000);
+  Serial.println("Hello World");
+
 }
