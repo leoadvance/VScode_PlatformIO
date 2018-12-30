0c72a5f7c17a67d514958b2bb0e18a2cbb0e0a69
leoadvance <leo_advance@163.com>
2018-12-30 23:52:40 +0800

    实现Wi-Fi连接管理

--- ESP32Client/src/main.cpp
+++ ESP32Client/src/main.cpp
@@ -4,45 +4,103 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-const char* ssid = "LEO_ASUS";
-const char* password =  "bhu8vgy7nji9";
+// const char* ssid = "LEO_ASUS";
+// const char* password =  "bhu8vgy7nji9";
+const char* ssid = "LEO's iPhoneX";
+const char* password =  "8765432a";
 int LED_BUILTIN = 2;
 // 默认Wi-Fi 未连接
 wl_status_t WiFiStatus = WL_DISCONNECTED;
 
+#define LED_CLOSE   digitalWrite(LED_BUILTIN, LOW)
+#define LED_OPEN   digitalWrite(LED_BUILTIN, HIGH)
+
+char *WiFiStatusToString(int status)
+{
+    switch(status)
+    {
+        case 0:
+            return (char*)"WL_IDLE_STATUS"; 
+        case 1:
+            return (char*)"WL_NO_SSID_AVAIL"; 
+        case 2:
+            return (char*)"WL_SCAN_COMPLETED"; 
+        case 3:
+            return (char*)"WL_CONNECTED"; 
+        case 4:
+            return (char*)"WL_CONNECT_FAILED"; 
+        case 5:
+            return (char*)"WL_CONNECTION_LOST"; 
+        case 6:
+            return (char*)"WL_DISCONNECTED";     
+
+        case 255:
+
+        default:
+            return (char*)"WL_STATUS_ERROR";       
+    }
+}
 // 500ms定时任务
 void taskLED(void * parameter )
 {
-    int i = 0;
+    //int i = 0;
     pinMode(LED_BUILTIN, OUTPUT);
+    LED_CLOSE;
     delay(500);
     while(1)
     {
-        Serial.printf("taskLED: %d\r\n", i++);
-        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(500);                       // wait for a second
-        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-        delay(500);  
+        // 联网后蓝灯常亮 
+        if (WiFiStatus == WL_CONNECTED)
+        {
+            LED_OPEN;
+        }
+        else
+        {
+            LED_CLOSE;
+        }
+        delay(1000);  
     }
 }
 
-// 500ms定时任务
+// Wi-Fi连接管理
 void taskWiFi(void * parameter )
 {
-    int i;
+    int i = 0;
+    int j = 0;
     delay(500);
     while(1)
     {
-    
-        if (WiFi.isConnected() == false)
+        j++;
+        // 获取Wi-Fi状态
+        WiFiStatus = WiFi.status();
+        if (j == 10)
         {
+            j = 0;
+            Serial.printf("WiFiStatus = %s\r\n", WiFiStatusToString(WiFiStatus));
+        }     
+        if (WiFiStatus != WL_CONNECTED)
+        {
+            
+            Serial.printf("Connecting to Wi-Fi...\r\n");
 
-            while (WiFi.status() != WL_CONNECTED) 
+            //连接WiFi
+            WiFi.begin(ssid, password);
+            i = 20;
+            do
             {
                 delay(1000);
-                Serial.println("Connecting to WiFi..");
+                WiFiStatus = WiFi.status();
+                Serial.printf(" WiFi.begin WiFiStatus = %s\r\n", WiFiStatusToString(WiFiStatus));
+                i--;
+
+            }while((WiFiStatus != WL_CONNECTED) && (i > 0));
+
+            if (WiFiStatus == WL_CONNECTED)
+            {
+                Serial.printf("Wi-Fi connect success!\r\n");
             }
         }
+        delay(1000);
     }
 }
 
@@ -52,8 +110,8 @@ void setup()
     Serial.begin(115200);
     delay(200);
     Serial.println("ESP32 Client Start……");
-    xTaskCreate(taskLED, "taskLED", 2000, NULL, 1, NULL);
-    xTaskCreate(taskWiFi, "taskWiFi", 8000, NULL, 2, NULL);
+    xTaskCreate(taskLED, "taskLED", 2048, NULL, 1, NULL);
+    xTaskCreate(taskWiFi, "taskWiFi", 8192, NULL, 2, NULL);
 
 }
 
