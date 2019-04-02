9cc09ba222ac7ac2951f28adcb87da4a0397d00c
leoadvance <leo_advance@163.com>
2019-04-02 16:41:03 +0800

    新建Arduino UNO工程

---
+++ Arduino_UNO_Test/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200); 
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Hello World!\n");
+  delay(1000);
+  
+}
