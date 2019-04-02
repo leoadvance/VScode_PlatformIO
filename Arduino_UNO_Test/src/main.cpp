afcdb787c101f91fe2e3699c3feac16b6c44c0ce
leoadvance <leo_advance@163.com>
2019-04-02 19:14:30 +0800

    1s读取一次ADC

--- Arduino_UNO_Test/src/main.cpp
+++ Arduino_UNO_Test/src/main.cpp
@@ -1,13 +1,23 @@
 #include <Arduino.h>
-
+int analogPin = A0;
+long val = 0;
+float tempVal = 0;
+char buffer[256];
 void setup() {
+  // 配置串口波特率
   Serial.begin(115200); 
   // put your setup code here, to run once:
+  Serial.print("Hello World!\n");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Hello World!\n");
+  
+  val  = analogRead(analogPin);  // read the input pin
+  tempVal = (val * 5000) / 1023;
+  val = tempVal;
+  sprintf(buffer, "Current voltage = %ld.%03ld V\n", val / 1000, val % 1000);
+  Serial.print(buffer);
   delay(1000);
   
 }
